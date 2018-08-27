8b4c3cbf9f75142dda47aec50b81c29f4d39eaef
KissT <github@kisst.net>
2018-08-27 21:28:57 +0100

    a bit of cleanup

--- docker-setup.sh
+++ docker-setup.sh
@@ -7,7 +7,7 @@ apt-get update -qq
 
 if [ "$sys_r" -ne "0" ]
 then
-  xargs apt-get install -y < system-requirements.txt || exit 1
+  xargs apt-get --no-install-recommends install -y < system-requirements.txt || exit 1
 fi
 
 pip3 install --upgrade pip
@@ -16,3 +16,6 @@ if [ "$pip_r" -ne "0" ]
 then
   pip3 install -r pip-requirements.txt || exit 1
 fi
+
+rm -rf /var/lib/apt/lists/*
+rm -rf /var/cache/apt/archives
